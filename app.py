b57a2985a10c18b39e628f59014f01d707b9c7c7
Klein2003 <0987654321.tyuil@gmail.com>
2026-09-14 11:18:51 +0700

    1

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 import streamlit as st
+
+st.title("Dashboard Group B")
+
+st.header("สมาชิก")
+st.subheader("นายรชต ปานงาม")
+st.subheader("นายรชต ปานงาม")
+st.subheader("นายรชต ปานงาม")
+st.subheader("นายรชต ปานงาม")
